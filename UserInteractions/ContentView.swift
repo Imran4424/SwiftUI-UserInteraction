b611bda3464a5413f87eee53ca1c3353ca5870c7
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-05 09:13:27 +0600

    project init

---
+++ UserInteractions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserInteractions
+//
+//  Created by Shah Md Imran Hossain on 5/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserInteractions/UserInteractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInteractionsApp.swift
+//  UserInteractions
+//
+//  Created by Shah Md Imran Hossain on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UserInteractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
